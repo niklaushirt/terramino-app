b1a341f10af40effd50770ea94c3b79eb17cc202
Niklaus Hirt <nikh@ch.ibm.com>
2025-10-03 12:07:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "web_sg" {
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.example.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-2a"
+  availability_zone = "eu-central-1a"
   map_public_ip_on_launch = true
 
   tags = {
